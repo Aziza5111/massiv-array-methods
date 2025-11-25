2ec85f683a4dd42a4b5e756ceca71d8f6dcabc0d
Aziza Rahimova <rahimovaaziza358@gmail.com>
2025-11-25 00:55:59 -0800

    lesson is end

---
+++ app.js
@@ -0,0 +1,26 @@
+let myArr = [1, 2, 30, 4, 5];
+// myArr.push(6);
+// myArr.pop();
+// myArr.unshift(0);
+// myArr.shift();
+
+// let newArr = myArr.map((n) => n * 2);
+
+// let adults = myArr.filter((age) => age >= 18);
+
+// let sum = myArr.reduce((acc, curr) => acc * curr, 1);
+
+// let findAge = myArr.find((age) => age > 10);
+
+// let even = myArr.some((n) => n % 2 === 0);
+
+// let odd = myArr.every((n) => n % 2 === 0);
+
+// myArr.sort((a, b) => a - b);
+// myArr.sort((a, b) => b - a);
+
+myArr.forEach((n, index) => {
+  console.log(n);
+});
+
+console.log(myArr);
